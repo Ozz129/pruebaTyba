bcb6febc4313f79ae5c3b005b327e6419e57d3a0
Fabian Martinez <fmartinez122990@gmail.com>
2022-06-04 22:54:08 -0500

    Hide secret fro external API

--- controllers/city.js
+++ controllers/city.js
@@ -22,7 +22,7 @@ const getData = async (req, res) => {
         url: 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities/Q65/locatedIn',
         headers: {
           'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
-          'X-RapidAPI-Key': '33ef511d77msh96116ca5fe36098p12a091jsn4d58247d3bff'
+          'X-RapidAPI-Key': process.env.RAPID_API_KEY
         }
       };
       
